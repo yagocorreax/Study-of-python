0d4a9918c2f55c282093254a13f7b12d799e28a1
Yago Ferreira <yagocmferreira9@gmail.com>
2025-12-05 13:31:27 -0300

    exercício Área a ser pintada
    Nesse exercício foi proposto calcular a área de uma parede a ser pintada e a quantidade de tinta necessária para fazer este serviço.

---
+++ lista lógica de programação/exercicio10.py
@@ -0,0 +1,5 @@
+largura = float(input("Qual a largura da parede?"))
+altura = float(input("Qual a altura da parede?"))
+area = (largura * altura)
+tinta = (area / 2)
+print(f"A área a ser pintada será de {area} e a quantidade de tinta necessária para o serviço será de {tinta} litros.")
