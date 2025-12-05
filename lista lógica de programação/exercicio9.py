2986d778a275e52a245675d3e2b3a01aa0352279
Yago Ferreira <yagocmferreira9@gmail.com>
2025-12-05 13:29:32 -0300

    Conversão moedas
    Neste exercício foi proposto para pegar um valor em real e exibir quantos dólares você consegue com aquele determinado valor.

---
+++ lista lógica de programação/exercicio9.py
@@ -0,0 +1,4 @@
+dinheiro = float(input("Quanto de dinheiro você tem na carteira:"))
+Dolares = dinheiro / 3,45
+print(f"Com esse R$ {dinheiro} em reais, você pode comprar US {Dolares} dólares!")
+                 
