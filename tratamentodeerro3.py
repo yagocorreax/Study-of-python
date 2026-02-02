92665b2486fea7da1b07cb48148dbd0cc623e570
Yago Ferreira <yagocmferreira9@gmail.com>
2026-02-02 11:11:42 -0300

    Delete tratamentodeerro3.py

--- tratamentodeerro3.py
+++
@@ -1,8 +0,0 @@
-try:
-    number1 = float(input("Digite um valor:"))
-    number2 = float(input("Digite outro valor:"))
-    divisao = number1 / number2
-except ZeroDivisionError:
-    print("Divisão por zero não é permitida, usuário. Divisão inválida!")
-else:
-    print(f"Divisão bem sucedida, o resultado é {divisao}")
