98fad843121461d462e15a61981e506518a3a4e8
Yago Ferreira <yagocmferreira9@gmail.com>
2026-02-02 11:11:59 -0300

    Delete tratamentodeerro2.py

--- tratamentodeerro2.py
+++
@@ -1,7 +0,0 @@
-try:
-    numbe1 = int(input("Digite o primeiro valor:"))
-    number2 = int(input("Digite o segundo valor:"))
-    soma = (numbe1 + number2)
-    print(soma)
-except ValueError:
-    print("Entrada inválida!")
